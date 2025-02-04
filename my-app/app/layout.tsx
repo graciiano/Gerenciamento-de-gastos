61c28a0ea9f0c2c957962369f523e5f068c738f4
Diogo <Diogograciiano@gmail.com>
2025-02-04 18:51:22 -0300

    Feat: ajustando icone e nome da pagina.

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,8 +1,17 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils"
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"]});
+
+
+
+
+export const metadata: Metadata = {
+  title: "Finances",
+  description: "Controle suas financias",
+};
 
 
 export default function RootLayout({
